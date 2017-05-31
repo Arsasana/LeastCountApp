cd82a7a8ad290ade4bf18be6b68d9c9a112584bb
Ravinderz <ravinderz@outlook.com>
2017-05-31 17:00:35 +0530

    added nav component

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongojs = require('mongojs');
 const mongoose = require('mongoose');
 const config = require('./config');
 const user   = require('./src/models/User');
-console.log(user);
+const game   = require('./src/models/Game');
 
 const app = express();
 
@@ -27,8 +27,7 @@ routes.get('/test', function (req, res) {
 
 routes.post('/register',function(req,res){
 	var host = req.body.host;
-	var token = uuidV1();
-	var newUser = new User({
+	var newUser = new user({
 		firstName : req.body.firstName,
 		lastName : req.body.lastName,
 		email : req.body.email,
@@ -52,6 +51,65 @@ routes.post('/register',function(req,res){
 				res.json({success:true, message : 'User had been registered successfully'});
 			}
 		});
+});
+
+//authenticate a user
+routes.post('/login',function(req,res){
+	user.findOne({
+	email : req.body.email,
+	'isActive':true
+	},function(err,user){
+		if(err) throw err;
+		if(!user){
+			res.json({success:false,message : 'Authentication failed! User not found'});
+		}else if(user){
+			
+			 // check if password matches
+			 console.log(user);
+			 console.log(user.email);
+			if(user.password != req.body.password){
+			res.json({success:false, message : 'Authentication failed! invalid username and password'});
+			}else{
+				
+				 // if user is found and password is right
+				
+				
+				// return the information including token as JSON
+				res.json({
+					success:true,
+					message : 'user login successfull',
+					obj : user
+				});
+			}
+		}
+	});
+});
+
+routes.post('/createGame',function(req,res){
+	var newGame = new game({
+		gameName : req.body.gameName,
+		playersCount : req.body.playersCount,
+		gameScore : req.body.gameScore,
+		players :req.body.players
+	});
+		
+		newGame.save(function(err){
+			 if(err) {
+				console.log('Error Inserting New Data');
+				if (err.name === 'ValidationError') {
+					for (field in err.errors) {
+					console.log(err.errors[field].message); 
+					}
+				}
+				if(err.name === 'MongoError' && err.code === 11000){
+					console.log("mongo error");
+					return res.json({success:false,message:"error"});
+				}
+			}else{
+				res.json({success:true, message : 'game created successfully'});
+			}
+		});
+		
 });
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule,Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+
+const ROUTES = [
+  {
+    path: '',
+    redirectTo: 'app',
+    pathMatch: 'full'
+  },
+  
+  {
+    path:'home',
+    component: AppComponent
+  }
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+	RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
