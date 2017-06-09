21780a8d3b67f61cd9bad59079ce297ef6fe9e93
Ravinder Singh <ravinderz@outlook.com>
2017-06-09 20:49:44 +0530

    fixed issue in game component

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -6,85 +6,89 @@ import { PlayerNameService} from '../player-name.service';
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
 })
-export class GameComponent implements OnInit{
-	
-	playerNames = [];
-	playerScores = [];
-	individualPlayerScores=[];
-	dummyarray = [];
-	singleRoundScore = [];
-	playerTotalScore = [];
-	gameScore: number;
-	gameName: string;
-	model:any ={};
-	pattern = "^([0-9]|0[0-9]|1[0-9]|2[0-9]|3[0-9]|40|XX|xx)$";
-	
-  constructor(	private playerNameService : PlayerNameService
-				) {
-	this.playerNames= this.playerNameService.getPlayerNames();
-	this.gameScore = this.playerNameService.getGameScore();
-	this.gameName = this.playerNameService.getGameName();
-	
-	
+export class GameComponent implements OnInit {
+
+   playerNames = [];
+   playerScores = [];
+   individualplayerScores = [];
+   dummyarray = [];
+   singleRoundScore = [];
+   playerTotalScore = [];
+   gameScore: number;
+   gameName: string;
+  model: any = {};
+  pattern = '^([0-9]|0[0-9]|1[0-9]|2[0-9]|3[0-9]|40|XX|xx)$';
+
+  constructor(	private playerNameService: PlayerNameService	) {
+  this.playerNames = this.playerNameService.getPlayerNames();
+  this.gameScore = this.playerNameService.getGameScore();
+  this.gameName = this.playerNameService.getGameName();
+
   }
 
   ngOnInit() {
-	  this.createPlayerScoreArr();
-	  console.log(this.individualPlayerScores);
+    this.createInitialArrays();
   }
 
 
-  createPlayerScoreArr(){
-	   let sumArr = [];
-	  
-	  for(let i = 0 ; i < this.playerNames.length;i++){
-		  sumArr.push([]);
-	  }
-	 this.individualPlayerScores = sumArr;
-	  
+    createInitialArrays() {
+       const sumArr = [];
+
+      for ( let i = 0 ; i < this.playerNames.length; i++) {
+        sumArr.push([]);
+      }
+     this.individualplayerScores = sumArr;
+
+    }
+
+    addplayerScores() {
+       const length = this.playerNames.length;
+       const tempArr = this.playerScores;
+       console.log('tempArr length' + tempArr.length);
+       for (let i = 0 ; i < tempArr.length; i++) {
+      for (let j = 0; j < length; j++ ) {
+           this.individualplayerScores[j].push(tempArr[i][j]);
+        }
+      }
+       console.log(this.individualplayerScores);
+       this.calculateTotalScore();
+       this.createInitialArrays();
+    }
+
+    calculateTotalScore() {
+      const tempArr = this.individualplayerScores;
+       for ( let i = 0 ; i < tempArr.length; i++) {
+      let sum = 0;
+      for ( let j = 0; j < tempArr[i].length; j++ ) {
+           sum += tempArr[i][j];
+        }
+        this.playerTotalScore[i] = sum;
+        console.log(this.playerTotalScore);
+      }
+
+    }
+
+    onBlurMethod(event: any) {
+        const playerScore = event.target.value;
+        const id = Number(event.target.attributes.id.value);
+      this.singleRoundScore = this.singleRoundScore.filter(Boolean);
+      if ( playerScore > 40  || playerScore < 0 ) {
+        console.log('invalid entry');
+      } else if ( playerScore || playerScore === 0 ) {
+        if ( this.singleRoundScore.length < this.playerNames.length - 1) {
+          this.singleRoundScore.splice(id, 0, Number(playerScore));
+          console.log( 'singleRoundScore : ', this.singleRoundScore);
+          console.log( 'length of singleroundarr inside if : ', this.singleRoundScore.length);
+        }else {
+          console.log('inside else');
+          this.singleRoundScore.splice(id, 0, Number(playerScore));
+          this.playerScores.push(this.singleRoundScore);
+          this.singleRoundScore = [];
+          this.dummyarray = [];
+          this.addplayerScores();
+        }
+    }else {
+        console.log('invalid entry');
+      }
+    }
   }
-  
-  addPlayerScores(){
-	   let length = this.playerNames.length;
-	   let tempArr = this.playerScores;
-	   console.log("tempArr length"+tempArr.length);
-	   for(let i = 0 ; i < tempArr.length;i++){
-		for(let j = 0; j < length;j++ ){
-				 this.individualPlayerScores[j].push(tempArr[i][j]);
-		  }
-	  }
-	   console.log(this.individualPlayerScores);
-	   this.calculateTotalScore();
-	   this.createPlayerScoreArr();
-  }
-  
-   calculateTotalScore(){
-	  let tempArr = this.individualPlayerScores;
-	   for(let i = 0 ; i < tempArr.length;i++){
-		let sum = 0;
-		for(let j = 0; j < tempArr[i].length;j++ ){
-				 sum += tempArr[i][j]; 
-				
-		  }
-		   console.log(sum);
-		  this.playerTotalScore[i] = sum;
-		  console.log(this.playerTotalScore);
-	  }
-	  	  
-  } 
-  
-  onBlurMethod(playerScore){
-	 
-	  if(this.singleRoundScore.length < this.playerNames.length-1){
-		this.singleRoundScore.push(Number(playerScore));
-	  }else{
-		  console.log("inside else");
-		  this.singleRoundScore.push(Number(playerScore));
-		  this.playerScores.push(this.singleRoundScore);
-		  this.singleRoundScore = [];
-		  this.dummyarray=[];
-		  this.addPlayerScores();
-	  }
-	  
-  }
-}
