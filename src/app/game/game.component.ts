95371c2d4d46acd009d088b49c4e947dde453148
ravinder <ravinderz@outlook.com>
2017-06-12 21:40:57 +0530

    modified game component

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PlayerNameService} from '../player-name.service';
+import {consoleTestResultHandler} from "tslint/lib/test";
 
 @Component({
   selector: 'app-game',
@@ -8,7 +9,8 @@ import { PlayerNameService} from '../player-name.service';
 })
 export class GameComponent implements OnInit {
 
-   playerNames = [];
+  playerNames = ['xx','yy','zz'];
+   //playerNames = [];
    playerScores = [];
    individualplayerScores = [];
    dummyarray = [];
@@ -20,10 +22,11 @@ export class GameComponent implements OnInit {
   pattern = '^([0-9]|0[0-9]|1[0-9]|2[0-9]|3[0-9]|40|XX|xx)$';
 
   constructor(	private playerNameService: PlayerNameService	) {
-  this.playerNames = this.playerNameService.getPlayerNames();
+  /*this.playerNames = this.playerNameService.getPlayerNames();
   this.gameScore = this.playerNameService.getGameScore();
-  this.gameName = this.playerNameService.getGameName();
-
+  this.gameName = this.playerNameService.getGameName();*/
+    this.gameScore = 300;
+    this.gameName = 'testing';
   }
 
   ngOnInit() {
@@ -60,28 +63,44 @@ export class GameComponent implements OnInit {
        for ( let i = 0 ; i < tempArr.length; i++) {
       let sum = 0;
       for ( let j = 0; j < tempArr[i].length; j++ ) {
-           sum += tempArr[i][j];
+        if (tempArr[i][j] === 'NA' || tempArr[i][j] === 'XX' || tempArr[i][j] === 'xx' || tempArr[i][j] === 'SW' || tempArr[i][j] === 'sw') {
+          tempArr[i][j] = 0;
+          sum += tempArr[i][j];
+        } else {
+          sum += tempArr[i][j];
         }
+      }
         this.playerTotalScore[i] = sum;
         console.log(this.playerTotalScore);
       }
 
     }
 
-    onBlurMethod(event: any) {
+    onBlurMethod(event: any, scoreForm: any) {
         const playerScore = event.target.value;
         const id = Number(event.target.attributes.id.value);
+        console.log(scoreForm.controls);
+        scoreForm.controls.playerscore['touched'] = false;
+        scoreForm.controls.playerscore['untouched'] = true;
       this.singleRoundScore = this.singleRoundScore.filter(Boolean);
       if ( playerScore > 40  || playerScore < 0 ) {
         console.log('invalid entry');
       } else if ( playerScore || playerScore === 0 ) {
         if ( this.singleRoundScore.length < this.playerNames.length - 1) {
+          if (playerScore === 'NA' || playerScore === 'XX' || playerScore === 'xx' || playerScore === 'SW' || playerScore === 'sw') {
+            this.singleRoundScore.splice(id, 0, playerScore);
+          } else {
           this.singleRoundScore.splice(id, 0, Number(playerScore));
+          }
           console.log( 'singleRoundScore : ', this.singleRoundScore);
           console.log( 'length of singleroundarr inside if : ', this.singleRoundScore.length);
         }else {
           console.log('inside else');
+          if (playerScore === 'NA' || playerScore === 'XX' || playerScore === 'xx' || playerScore === 'SW' || playerScore === 'sw') {
+            this.singleRoundScore.splice(id, 0, playerScore);
+          } else {
           this.singleRoundScore.splice(id, 0, Number(playerScore));
+          }
           this.playerScores.push(this.singleRoundScore);
           this.singleRoundScore = [];
           this.dummyarray = [];
